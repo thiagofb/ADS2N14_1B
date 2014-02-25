3161cd280417d3c5170e281ad255b0afc6bba14f
thiago fischborn <thiagofisch@gmail.com>
2014-02-24 23:41:59 -0300

    Programa Pessoa
    Este programa se trata de uma classe que descreve os dados de uma pessoa
    e os demonstra.  (Versão incompleta).

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+package t1;
+import java.util.*;
+public class Pessoa {
+
+	
+	public static void main(String[] args) {
+		Scanner t=new Scanner(System.in); 
+		 
+		String nome;
+		double peso;
+		double altura;
+		
+		
+		
+		System.out.println("Digite seu nome:");
+		nome=t.next();
+		System.out.println("Digite seu peso");
+		peso=t.nextDouble();
+		System.out.println("Digite sua altura");
+		altura=t.nextDouble();
+		
+		
+		
+		
+
+	}
+
+}
