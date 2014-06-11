6fa19d64459d2bc7e32ced0ef64e045b296b3328
thiago fischborn <thiagofisch@gmail.com>
2014-06-11 00:00:39 -0300

    projeto final
    trabalho de issue

---
+++ proj/src/Evento.java
@@ -0,0 +1,18 @@
+
+public class Evento {
+	
+	private String data;
+	private String comentario;
+	
+	Issues issue = new Issues();
+	
+	public evento(String data, String comentario){
+		this.data = data;
+		this.comentario=comentario;
+	}
+	
+	
+	
+	
+
+}
